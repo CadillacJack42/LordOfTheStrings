a377e277eca90ce2703623ec9b5db5783b602562
James Keicher <jamesdkeicher@gmail.com>
2024-11-22 16:05:35 -0800

    Fixed contact links to actually be links and not just images.

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -10,35 +10,58 @@ export const Tabs = ({ page }) => {
 
   return (
     <div id="tabs-container">
-      <span>
-        <img
+      <span id="contact-links-container">
+        <a
+          href="https://yelp.com/biz/the-lord-of-the-strings-los-angeles-2?utm_medium=badge_star_rating_reviews&amp;utm_source=biz_review_badge"
+          target="_blank"
+          rel="noreferer noreferrer"
           className="contact-links"
-          src="./yelp.jpg"
-          alt="Check out The Lord of the Strings on Yelp"
-          width="125"
-          height="65"
-        ></img>
-        <img
+        >
+          <img
+            src="./yelp.jpg"
+            alt="Check out The Lord of the Strings on Yelp"
+            className="contact-link-logos"
+          ></img>
+        </a>
+
+        <a
+          href="https://www.youtube.com/@gabe_teaches_guitar"
+          target="_blank"
+          rel="noreferer noreferrer"
           className="contact-links"
-          src="./Youtube.png"
-          alt="Subscribe to me on YouTube"
-          width="125"
-          height="65"
-        ></img>
-        <img
+        >
+          <img
+            src="./Youtube.png"
+            alt="Subscribe to me on YouTube"
+            className="contact-link-logos"
+          ></img>
+        </a>
+
+        <a
+          href="https://www.instagram.com/gabe_teaches_guitar"
+          target="_blank"
+          rel="noreferer noreferrer"
           className="contact-links"
-          src="/Instagram.png"
-          alt=""
-          width="125"
-          height="65"
-        ></img>
-        <img
+        >
+          <img
+            src="./Instagram.png"
+            alt="Follow me on Instagram"
+            className="contact-link-logos"
+          ></img>
+        </a>
+
+        <a
+          href="https://www.facebook.com/gabeteachesguitar"
+          target="_blank"
+          rel="noreferer noreferrer"
           className="contact-links"
-          src="./facelogo.jpg"
-          alt=""
-          width="125"
-          height="65"
-        ></img>
+        >
+          <img
+            src="./facelogo.jpg"
+            alt="Follow me on Facebook"
+            className="contact-link-logos"
+          ></img>
+        </a>
       </span>
       <div className="tabs-row">
         {page.tabs.map((tab, i) => {
